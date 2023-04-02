7ed9e103e204a414829ecda101b48b17b308145c
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-04-02 10:47:13 -0400

    Remove redundant 'repo:' header from key name

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   tfstate_s3_bucket_arn      = "arn:aws:s3:::tenzin-io"
   tfstate_dynamodb_table_arn = "arn:aws:dynamodb:us-east-1:130900203380:table/tenzin-io"
   repos = {
-    "repo:tenzin-io/cloudflare-dns" = {
+    "tenzin-io/cloudflare-dns" = {
       claim = "repo:tenzin-io/cloudflare-dns:ref:refs/heads/main"
     }
   }
