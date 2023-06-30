b52d6064ec46bea669fc910a5638546441eaca9c
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-06-30 09:19:36 -0400

    Use thumbprints provided by GitHub blog post

--- main.tf
+++ main.tf
@@ -11,10 +11,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+#
+# See this post for the official thumbprint list for github actions
+# https://github.blog/changelog/2023-06-27-github-actions-update-on-oidc-integration-with-aws/
+#
 resource "aws_iam_openid_connect_provider" "github_actions" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
 }
 
 resource "aws_iam_role" "github_actions_role" {
